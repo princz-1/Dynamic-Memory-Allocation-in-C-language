6991ce408f78bc69e67c5a939ac5aab4d9c6817b
Princz.1 <150322497+princz-1@users.noreply.github.com>
2023-11-17 14:41:32 +0530

    1-Malloc
    - takes number of bytes to be allocated & returns a pointer of type void
    
    - ptr = (int*)malloc(5*size(int));
      
      */

---
+++ 1-Malloc.c
@@ -0,0 +1,26 @@
+/* malloc()
+memory allocation
+
+- takes number of bytes to be allocated & returns a pointer of type void
+
+- ptr = (int*)malloc(5*size(int));
+  
+  */
+  
+#include <stdio.h>
+#include<stdlib.h>
+int main(void) {
+ int *ptr;
+  ptr = (int*)malloc(5*sizeof(int));
+
+  ptr[0] = 1;
+  ptr[1] = 3;
+  ptr[2] = 5;
+  ptr[3] = 7;
+  ptr[4] = 9;
+
+  for(int i=0; i<5; i++){
+    printf("%d\n",ptr[i]);
+  }
+  return 0;
+}
