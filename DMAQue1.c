a9adadfdad8e0e9465ef61a65b9dfe1fb4dc3191
Princz.1 <150322497+princz-1@users.noreply.github.com>
2023-11-17 14:46:47 +0530

    Dynamic Memory Allocation Question.1
    // Write a program to allocate memory to store 5 prices.

---
+++ DMAQue1.c
@@ -0,0 +1,19 @@
+// Write a program to allocate memory to store 5 prices.
+
+#include <stdio.h>
+
+int main(void) {
+float*ptr;
+  ptr = (float*)malloc(5*sizeof(float));
+
+  ptr[0] = 1;
+  ptr[1] = 3;
+  ptr[2] = 5;
+  ptr[3] = 7;
+  ptr[4] = 9;
+
+  for(int i=0; i<5; i++){
+    printf("%f\n",ptr[i]);
+  }
+  return 0;
+}
